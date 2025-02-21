f0853d4f4c767c689f7d8e8d09ef5609b31f09b4
cnochur <cnochursaunders@gmail.com>
2025-02-21 13:11:41 +0000

    added a contact form
    Backend for form still needs done

--- profile/app.py
+++ profile/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
+
+app = Flask(__name__)
+
 from views.index import index
 from views.projects import projects
 from views.contact import contact
 
-app = Flask(__name__)
+
 app.register_blueprint(index, url_prefix="/")
-app.register_blueprint(projects, url_prefix="/projects")
-app.register_blueprint(contact, url_prefix="/contact")
+app.register_blueprint(projects, url_prefix="/projects/")
+app.register_blueprint(contact, url_prefix="/contact/")
 
 
 if __name__ == "__main__":
--- profile/views/contact.py
+++ profile/views/contact.py
@@ -5,3 +5,4 @@ contact = Blueprint("contact", __name__)
 @contact.route("/")
 def home():
     return render_template("contact.html")
+
