60d7d51c4715b42bfc7d1a1d9879ecc944e030b8
cnochur <cnochursaunders@gmail.com>
2025-02-21 15:22:03 +0000

    views updated

--- budget_app/app.py
+++ budget_app/app.py
@@ -1,28 +1,11 @@
 from flask import Flask, render_template
 import sqlite3
 
-db = sqlite3.connect("app.db")
-cursor = db.cursor()
-cursor.execute('''
-               CREATE TABLE IF NOT EXISTS monthly_budget(
-                   budget_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                   sal FLOAT,
-                   bills TEXT,
-                   necs TEXT,
-                   remaining FLOAT
-                   )''')
-cursor.execute('''
-               CREATE TABLE IF NOT EXISTS item(
-                   item_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   title TEXT,
-                   emoji TEXT,
-                   item_price FLOAT
-                   )''')
-db.commit()
-db.close()
-
 app = Flask(__name__)
+
+def get_db_connection():
+    db = sqlite3.connect("db/app.db")
+    return db
 
 @app.route("/")
 def home():
